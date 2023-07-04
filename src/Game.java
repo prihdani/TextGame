7334722eeb248c30e0015022280df9a6438001d1
Dani <prihoczki.daniel@gmail.com>
2023-07-04 07:49:13 +0200

    Created game panel/window

---
+++ src/Game.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game {
+
+    JFrame window;
+    public static void main(String[] args) {
+
+        new Game();
+    }
+    public Game(){
+        window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.getContentPane().setBackground(Color.BLUE);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+}
